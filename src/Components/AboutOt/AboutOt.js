3f9ecc05853d3fdd7224c186dab4e40167c25046
Oliwier Markiewicz <101707516+ol1mowski@users.noreply.github.com>
2023-03-21 15:55:46 +0100

    add_secons_section

--- src/Components/AboutOt/AboutOt.js
+++ src/Components/AboutOt/AboutOt.js
@@ -1,7 +1,17 @@
+import styles from './AboutOt.module.css';
+
 function AboutOt() {
     return(
-        <div>
-            About
+        <div className={styles.container}>
+            <div className={styles.bigDes}>
+                <span><span className={styles.color}>OT-Groupe</span> to startup, który chce zmienić sposób, w jaki prowadzone są kursy programowania.</span>
+            </div>
+            <br />
+            <br />
+            <br />
+            <div className={styles.smallDes}>
+                <span>Nasz kurs został stworzony, aby dostarczać zastrzyk solidnej wiedzy z zakresu programowania w języku JavaScript wszystkim naszym kursantą.</span>
+            </div>
         </div>
     )
 }
