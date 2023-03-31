2fca976cd8f0bb86d783dd6c389cb8f8160ba857
Oliwier Markiewicz <101707516+ol1mowski@users.noreply.github.com>
2023-03-31 12:53:47 +0200

    add_1024, 1440

--- src/Components/AboutOt/AboutOt.js
+++ src/Components/AboutOt/AboutOt.js
@@ -1,18 +1,43 @@
+import { useEffect } from 'react';
+import { animateScroll } from 'react-scroll';
 import styles from './AboutOt.module.css';
 
 function AboutOt() {
-    return(
-        <div className={styles.container}>
-            <div className={styles.bigDes}>
-                <span><span className={styles.color}>OT-Groupe</span> to startup, który chce zmienić sposób, w jaki prowadzone są kursy programowania.</span>
-            </div>
-            <br />
-            <br />
-            <br />
-            <div className={styles.smallDes}>
-                <span>Nasz kurs został stworzony, aby dostarczać zastrzyk solidnej wiedzy z zakresu programowania w języku JavaScript wszystkim naszym kursantą.</span>
-            </div>
-        </div>
-    )
+  const scrollToElement = () => {
+    animateScroll.scrollToElement('#about', {
+      duration: 500,
+      delay: 100,
+      smooth: 'easeInOutQuart',
+    });
+  };
+
+  useEffect(() => {
+    const about = document.getElementById('about');
+    if (about) {
+      about.addEventListener('mouseenter', scrollToElement);
+    }
+    return () => {
+      if (about) {
+        about.removeEventListener('mouseenter', scrollToElement);
+      }
+    };
+  }, []);
+
+  return (
+    <div id="about" className={styles.container}>
+      <div className={styles.bigDes}>
+        <span>
+          <span className={styles.color}>OT-Groupe</span> to startup, który chce zmienić sposób, w jaki prowadzone są kursy programowania.
+        </span>
+      </div>
+      <br />
+      <br />
+      <br />
+      <div className={styles.smallDes}>
+        <span>Nasz kurs został stworzony, aby dostarczać zastrzyk solidnej wiedzy z zakresu programowania w języku JavaScript wszystkim naszym kursantą.</span>
+      </div>
+    </div>
+  );
 }
+
 export default AboutOt;
